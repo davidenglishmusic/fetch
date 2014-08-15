ca586c1c13de81454df2eae7dcd8189ade85ca77
David English <david@operaonfilm.com>
2014-08-15 11:02:13 -0700

    Added the ability to return mulitple values

--- lib/fetch.ex
+++ lib/fetch.ex
@@ -12,14 +12,51 @@ defmodule Fetch do
   end
 
   def getValue(fileBody, element) do
-    [[source, value]] = Regex.scan(~r/<#{element}>(.*?)<\/#{element}>/, fileBody)
+    results = Regex.scan(~r/<#{element}>(.*?)<\/#{element}>/, fileBody)
+    do_getValue(results)
+  end
+
+  defp do_getValue(results) when length(results) >= 2 do
+    getValues(results, [])
+  end
+
+  defp do_getValue(results) do
+    [[source, value]] = results
     value
   end
 
+  defp getValues(results, acc) when results == [] do
+    Enum.reverse(acc)
+  end
+
+  defp getValues(results, acc) do
+    value = Enum.at(results, 0) |> tl()
+    newAcc = value ++ acc
+    getValues(tl(results), newAcc)
+  end
+
   def getAttribute(fileBody, attribute, element) do
-    [[source, elementLine]] = Regex.scan(~r/<#{element}(.*?)>/, fileBody)
+    result = Regex.scan(~r/<#{element}(.*?)>/, fileBody)
+    do_getAttribute(attribute, result)
+  end
+
+  defp do_getAttribute(attribute, result) do
+    [[source, elementLine]] = result
     [[source, attribute]] = Regex.scan(~r/#{attribute}="(.*?)"/, elementLine)
     attribute
+  end
+
+  def getListLength(list) do
+    do_getListLength(list, 0)
+  end
+
+  defp do_getListLength(list, acc) when list == [] do
+    acc
+  end
+
+  defp do_getListLength(list, acc) do
+    newList = tl(list)
+    do_getListLength(newList, acc + 1)
   end
 
 end
--- test/fetch_test.exs
+++ test/fetch_test.exs
@@ -13,6 +13,14 @@ defmodule FetchTest do
     assert Fetch.getValue("<software>MuseScore 1.3</software>", "software") == "MuseScore 1.3"
   end
 
+  test "gets value of two notes" do
+    assert Fetch.getValue("<note>A</note><note>B</note>", "note") == ["A", "B"]
+  end
+
+  test "gets value of three notes" do
+    assert Fetch.getValue("<note>A</note><note>B</note><note>C</note>", "note") == ["A", "B", "C"]
+  end
+
   test "gets attribute of bar" do
     assert Fetch.getAttribute("<score-part id=\"P1\">", "id", "score-part") == "P1"
   end
